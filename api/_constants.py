7bd03b264c538202172cf21bf3f2ba8940424c6a
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-05-09 18:58:33 +0530

    single server only

--- api/_constants.py
+++ api/_constants.py
@@ -3,7 +3,7 @@ from flask import Response
 
 METHODS = ["get", "post", "patch", "put", "delete"]
 
-AVAILABLE = ["s1.halocrypt.com", "s2.halocrypt.com", "s3.halocrypt.com"]
+AVAILABLE = ["s1.halocrypt.com"]  # , "s2.halocrypt.com", "s3.halocrypt.com"]
 
 
 DEALER_KEY = environ["DEALER_KEY"]
